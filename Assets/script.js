61260e5ac247111ed128128b5ad194f18372345b
pup-trainr <tarheelchick59@yahoo.com>
2020-12-23 21:41:29 -0500

    initial code

---
+++ Assets/script.js
@@ -0,0 +1,157 @@
+// a variable for start time
+let secondsLeft = 76;	
+
+//the element that displays the time	
+let timer = document.getElementById("timer");
+
+//div for high scores
+let scoresDiv = document.getElementById("scores-div");
+
+let buttonsDiv = document.getElementById("buttons")
+
+//button for high scores	
+let viewScoresBtn = document.getElementById("view-scores")
+
+//start button div	
+let startButton = document.getElementById("start-button");
+startButton.addEventListener("click", setTime);
+
+// variable for the questions title	
+var questionDiv = document.getElementById("question-div");
+
+// div to hold the results	
+let results = document.getElementById("results");
+
+
+// div for the choices
+var choices = document.getElementById("choices");
+
+
+// array of divs where the multiple choice questions will display	
+//var choiceArr = [choices0, choices1, choices2, choices3];	
+
+
+// an array to store high scores	
+let emptyArray = [];	
+
+
+//the array of high scores from local storage
+let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
+
+
+// which question the user is on
+&& -44,8 +39,6 && var questionCount = 0;
+
+//keeping score	
+let score = 0	
+
+
+//Timer starts when the user clicks startButton (see above).
+function setTime() {	
+  displayQuestions();
+&& -63,66 +56,70 && function setTime() {
+
+//function to load the questions on the page
+function displayQuestions() {	
+  removeEls(startButton);
+        
+  if (questionCount < questions.length) {
+    questionDiv.innerHTML = questions[questionCount].title;
+    choices.textContent = "";
+
+  for (let i = 0; i < questions [questionCount].multiChoice.length; i++) {
+      let el = document.createElement ("button");
+      el.innerText = questions [questionCount].multiChoice[i];
+      el.setAttribute ("data-id", i);
+      el.addEventListener ("click", function (event) {
+          event.stopPropagation ();
+
+          if (el.innerText=== questions[questionCount].answer) {
+              score + = secondsLeft;
+          } else {
+              score - = 10;
+              secondsLeft = secondsLeft - 15;
+          }
+          
+        questionDiv.innerHTML = "";
+
+        if (questionCount === questions.length) {
+            return;
+          } else {
+              questionCount ++; 
+              displayQuestions ();
+            }
+        });
+          choices.append(el);
+        }
+      }
+    }   
+function captureUserScore() {	
+  timer.remove();	  
+  choices.textContent = "";
+
+
+
+  let initialsInput = document.createElement("input");	 
+  let postScoreBtn = document.createElement("input");	  
+
+
+  results.innerHTML = `You scored ${score} points! Enter initials: `;
+  initialsInput.setAttribute("type", "text");
+  postScoreBtn.setAttribute("type", "button");
+  postScoreBtn.setAttribute("value", "Post My Score!");
+  postScoreBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+
+  let initials = initialsInput.value;
+  let userAndScore = {
+    initials: initials,
+    score: score,
+    };
+
+    scoresArray.push(userAndScore);
+    saveScores(scoresArray);
+    displayAllScores();
+    clearScoresBtn();
+    goBackBtn();
+    viewScoresBtn.remove();
+   });
+  results.append(initialsInput);
+  results.append(postScoreBtn);
+
+   }
+
+const saveScores = (array) => {	
+    window.localStorage.setItem ("highScores", JSON.stringify (array));
+  }
+
+const defineScoresArray = (arr1, arr2) => {
+    if(arr1 !== null) {
+        return arr1
+    } else {
+        return arr2
+    }
+}
+
+const removeEls = (...els) => {
+    for (let el of els) el.remove ();
+}
+
+function displayAllScores () {
+    removeEls (timer, startButton, results);
+    let scoresArray = defineScoresArray(storedArray, emptyArray);
+
+    scoresArray.forEach(obj => {
+        let initials = obj.initials;
+        let storedScore = obj.score;
+        let resultsP = document.createElement("p");
+        resultsP.innerText = '${initials}: ${storedScore}';
+        scoresDiv.append(resultsP);
+    });
+
+}
+
+
+function viewScores() {
